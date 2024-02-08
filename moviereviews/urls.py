42bc8775767363674551f584a754a5d49e5608f9
pollorest <jrestrep32@eafit.edu.co>
2024-02-08 12:35:01 -0500

    Peliculas subidas

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
     path('about/', movieViews.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
