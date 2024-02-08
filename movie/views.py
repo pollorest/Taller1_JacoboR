bc7595de4d1ad46defe5901053bfcac3eaaafabe
pollorest <jrestrep32@eafit.edu.co>
2024-02-08 11:12:18 -0500

    nuevo cambio en archivo about

--- movie/views.py
+++ movie/views.py
@@ -8,4 +8,4 @@ def home(request):
     return render(request, 'home.html', {'name': 'Jacobo Restrepo'})
 
 def about(request):
-    return HttpResponse('<h1> Welcome to About Page </h1>')
+    return render(request, 'about.html')
