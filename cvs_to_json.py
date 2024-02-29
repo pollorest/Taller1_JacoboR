1af4b679774fa4412df5043b81abd10789ca665a
pollorest <jrestrep32@eafit.edu.co>
2024-02-29 12:00:06 -0500

    Peliculas agregadas a db

--- cvs_to_json.py
+++ cvs_to_json.py
@@ -6,7 +6,7 @@ df = pd.read_csv('movies_initial.csv')
 df.to_json('movies.json', orient='records')
 
 with open('movies.json', 'r') as file:
-    data = json.load(file)
+    movies = json.load(file)
 
 for i in range(100):
     movie = movies[i]
