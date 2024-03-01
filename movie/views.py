cfb3eda7132f5171d2881131b5761f7fe9e77c91
pollorest <jrestrep32@eafit.edu.co>
2024-03-01 00:04:37 -0500

    Grafica por años realizada

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 from .models import Movie
 
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
+
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1> Welcome to Home Page </h1>')
@@ -17,3 +22,40 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def statistics_view(request):
+    matplotlib.use('Agg')
+    years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')
+    movie_counts_by_year = {}
+    for year in years:
+        if year:
+            movies_in_year = Movie.objects.filter(year=year)
+        else:
+            movies_in_year = Movie.objects.filter(year__isnull=True)
+            year = "None"
+        count = movies_in_year.count()
+        movie_counts_by_year[year] = count
+    
+    bar_width = 0.5
+    bar_spacing = 0.5
+    bar_positions = range(len(movie_counts_by_year))
+
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+
+    plt.subplots_adjust(bottom=0.3)
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode('utf-8')
+
+    return render(request, 'statistics.html', {'graphic': graphic})
